7a84fd0d07a398f9da29555f78b38968f218a288
Rakibul Ahasan <59710234+PaponAhasan@users.noreply.github.com>
2023-07-11 20:38:00 +0600

    Update Barikoi- SWE - set 3.cpp

--- Barikoi- SWE - set 3.cpp
+++ Barikoi- SWE - set 3.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 int main() {
-	int tc;
-	cin >> tc;
-	while(tc--){
-	    int n;
-	    cin >> n;
-	    vector<int>ar(n);
-	    for(auto& i : ar) cin >> i;
-
-	    int i = 0, j = n - 1;
-    	for(i = 0; i < n - 1; i++){
-    	    if(ar[i] > ar[i + 1]) break;
-    	}
-
-    	for(j = n - 1; j > i; j--){
-    	    if(ar[j] > ar[j - 1]) break;
-    	}
+  int tc;
+  cin >> tc;
+  while(tc--){
+    int n;
+    cin >> n;
+    vector<int>ar(n);
+    for(auto& i : ar) cin >> i;
+    
+    int i = 0, j = n - 1;
+    for(i = 0; i < n - 1; i++){
+      if(ar[i] > ar[i + 1]) break;
+    }
+    
+    for(j = n - 1; j > i; j--){
+      if(ar[j] > ar[j - 1]) break;
+    }
     	    
-    	//cout << i << " " << j << "\n";
-    	if(i == j) printf("YES\n");
-    	else printf("NO\n");
-	}
-	return 0;
+    //cout << i << " " << j << "\n";
+    if(i == j) printf("YES\n");
+    else printf("NO\n");
+  }
+  return 0;
 }
